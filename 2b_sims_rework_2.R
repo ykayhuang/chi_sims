e0b21b7899bac394d4bca6416ef3defd9e73f409
ykayhuang <32692782+ykayhuang@users.noreply.github.com>
2019-04-17 13:07:58 -0500

    Update 2b_sims_rework_2.R
    chagne A*

--- 2b_sims_rework_2.R
+++ 2b_sims_rework_2.R
@@ -96,14 +96,14 @@ bf80 <- fread( paste0("4_r_output/sims_fixed_201904/sims_result_",80,".csv"),sep
                                  ))))
 #merge
 af1 <- rbind(af40,af80,bf40,bf80)
-af1$cond2 <- factor(af1$cond.note, levels = c("all random","bi_airport_5k","af","bi_mst_500","bi_ndvi_300",
-                                              "bi_mst_500 + bi_airport_5k","af+m","bi_airport_5k + bi_ndvi_300","af+n",
+af1$cond2 <- factor(af1$cond.note, levels = c("all random","af","bi_airport_5k","bi_mst_500","bi_ndvi_300",
+                                              "af+m","bi_mst_500 + bi_airport_5k","af+n","bi_airport_5k + bi_ndvi_300",
                                               "bi_mst_500 + bi_ndvi_300")) #sort the seq that i want, based on original varialbe's value
 levels(af1$cond2) <- c("None (simple random)","Air_B","Air_B*","MS_B","NDVI_B",
                        "Air_B + MS_B","Air_B* + MS_B",
                        "Air_B + NDVI_B","Air_B* + NDVI_B","MS_B + NDVI_B") 
 
-af2 <- 
+
 df3 <- rbind(clean_df2,a32df,t111df)
 
 ggplot(df3, aes(sample = dair,color=factor(df3$c))) +  stat_qq() 
@@ -138,3 +138,77 @@ rlist <- list()
 for (j in 1:length(pro_tested)){
   ggsave(file=paste0("4_r_output/figure_201904/figure22_",j,".png"), F_make_the_qq_grid(pro_tested[j]),width = 8,height = 5)
 }
+
+#so i finish the figure
+#table 1
+summary(clean_df2)
+summary(a32df)
+sapply(clean_df2,sd)
+sapply(a32df,sd)
+
+#table 2
+cor(clean_df2,method = "spearman")
+
+#table 3
+table(clean_df2$dair>5000)
+table(clean_df2$mst>0)
+table(clean_df2$ndvi<=0.35)
+
+#table 4(big one)
+#first to get the group value
+tb4g1 <- quantile(clean_df2$dair, probs = c(0, .2,.4,.6,.8,1), na.rm = T) %>% round(.,0) %>% as.numeric()
+tb4g2 <- quantile(clean_df2$mst[which(clean_df2$mst!=0)], probs = c(0, .25,.5,.75,1), na.rm = T) %>% round(.,0)%>% as.numeric()
+tb4g3 <- quantile(clean_df2$ndvi, probs = c(0, .2,.4,.6,.8,1), na.rm = T) %>% round(.,3) %>% as.numeric()
+
+tb4f1 <- function(x){c(table(x>=tb4g1[1]&x<=tb4g1[2])["TRUE"],
+                       table(x>=tb4g1[2]&x<=tb4g1[3])["TRUE"],
+                       table(x>=tb4g1[3]&x<=tb4g1[4])["TRUE"],
+                       table(x>=tb4g1[4]&x<=tb4g1[5])["TRUE"],
+                       table(x>=tb4g1[5]&x<=tb4g1[6])["TRUE"]
+                        )/length(x)*100 }
+
+tb4f2 <- function(x){c(table(x==0)["TRUE"],
+                       table(x>=tb4g2[1]&x<=tb4g2[2])["TRUE"],
+                       table(x>=tb4g2[2]&x<=tb4g2[3])["TRUE"],
+                       table(x>=tb4g2[3]&x<=tb4g2[4])["TRUE"],
+                       table(x>=tb4g2[4]&x<=tb4g2[5])["TRUE"]
+                        )/length(x)*100 }
+
+tb4f3 <- function(x){c(table(x>=tb4g3[1]&x<=tb4g3[2])["TRUE"],
+                   table(x>=tb4g3[2]&x<=tb4g3[3])["TRUE"],
+                   table(x>=tb4g3[3]&x<=tb4g3[4])["TRUE"],
+                   table(x>=tb4g3[4]&x<=tb4g3[5])["TRUE"],
+                   table(x>=tb4g3[5]&x<=tb4g3[6])["TRUE"]
+                    )/length(x)*100 }
+
+tb4_40 <- subset(af1,ln<8000)
+tb4m1 <- aggregate(tb4_40$dair,by=list(tb4_40$cond2),FUN=tb4f1) %>% as.matrix()
+tb4m2 <- aggregate(tb4_40$mst,by=list(tb4_40$cond2),FUN=tb4f2) %>% as.matrix()
+tb4m3 <- aggregate(tb4_40$ndvi,by=list(tb4_40$cond2),FUN=tb4f3) %>% as.matrix()
+tb4 <- rbind(tb4m1,tb4m2,tb4m3) %>% t
+write.table(tb4,file="4_r_output/figure_201904/tb4_40.csv",sep=",",row.names = F)
+
+tb4_80 <- subset(af1,ln>7999)
+tb4m1 <- aggregate(tb4_80$dair,by=list(tb4_80$cond2),FUN=tb4f1) %>% as.matrix()
+tb4m2 <- aggregate(tb4_80$mst,by=list(tb4_80$cond2),FUN=tb4f2) %>% as.matrix()
+tb4m3 <- aggregate(tb4_80$ndvi,by=list(tb4_80$cond2),FUN=tb4f3) %>% as.matrix()
+tb4 <- rbind(tb4m1,tb4m2,tb4m3) %>% t
+write.table(tb4,file="4_r_output/figure_201904/tb4_80.csv",sep=",",row.names = F)
+
+tb4f1(clean_df2$dair) %>% round(.,1)
+tb4f2(clean_df2$mst) %>% round(.,1)
+tb4f3(clean_df2$ndvi) %>% round(.,1)
+tb4f1(a32df$dair) %>% round(.,1)
+tb4f2(a32df$mst) %>% round(.,1)
+tb4f3(a32df$ndvi) %>% round(.,1)
+
+#table 4end
+
+#figure 5?
+df7 <- subset(af1,cond2=="None (simple random)"&ln>7999)
+df8 <- aggregate(df7$mst,by=list(df7$ln),max,na.rm=T)%>%.[2]-
+aggregate(df7$mst,by=list(df7$ln),min,na.rm=T)%>%.[2]
+
+df7 <- subset(af1,cond2=="Air_B*"&ln>7999)
+df9 <- aggregate(df7$mst,by=list(df7$ln),max,na.rm=T)%>%.[2]-
+  aggregate(df7$mst,by=list(df7$ln),min,na.rm=T)%>%.[2] 
